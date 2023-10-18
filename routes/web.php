e6baa3fec5b1da9724f75a96d12b489fadbbde80
unknown <stefanocopreni@hotmail.com>
2023-10-18 13:39:26 -0300

    create edit-post and show all

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,19 +11,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('home');
+    $posts = [];
+    if (auth()->check()) {
+        $posts = auth()->user()->usersCoolPosts()->latest()->get();
+    }
+    return view('home', ['posts' => $posts]);
 });
 
-Route::post ('/register', [UserController::class, 'register']);
+Route::post('/register', [UserController::class, 'register']);
 Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
 
 // Blog post related routes
-
 Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
